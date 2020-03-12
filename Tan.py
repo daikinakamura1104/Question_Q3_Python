7dd681f36a1df8d190a7722db32f2c1cad178d63
daikinakamura1104 <61182789+daikinakamura1104@users.noreply.github.com>
2020-03-12 18:43:36 +0900

    Delete

--- Tan.py
+++
@@ -1,9 +0,0 @@
-# coding:utf-8
-import math
-
-class Tan():
- 
-    def numofTan(self, num):
-        tan = math.tan(math.radians(num))
-        msg = "tan{}゜= {}"
-        print(msg.format(num,(round(tan,3))))
