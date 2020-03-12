27bf85256ff320bda4e803f81f140347fd52fe3e
junichi.enomoto <junichi.enomoto@tokyo.supersoftware.co.jp>
2020-03-12 13:56:31 +0900

    update

--- Call.py
+++ Call.py
@@ -17,8 +17,14 @@ cos = Cos.Cos()
 tan = Tan.Tan()
 call = Call()
 
-data = [15,30,45,60,75,90,105,120,135,150,165,180,195,210,225,240,255,270,285,300,315,330,345,360]
+# 360℃までの15℃毎の値をdata配列に格納する
+data_num = 15
+data = []
+while data_num < 375:
+    data.append(data_num)
+    data_num += 15
+
 for value in data:
     msg = "---　{}゜---"
     print(msg.format(value))
-    call.callofnum(value)
+    call.callofnum(value) 
